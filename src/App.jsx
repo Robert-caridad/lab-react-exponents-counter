73632b505dea27596d578f9c61fa264f7b6052f9
Robert-caridad <robertcaridad@gmail.com>
2024-07-28 19:49:41 +0200

    master

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,35 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
 
 
-function App () {
+  const calculateExp = (count, exponente) => Math.pow(count, exponente);
+
+
   return (
+
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+
+      <Counter count={count} increment={increment} decrement={decrement} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count} calculateExp={calculateExp} />
+        <ExponentThree count={count} calculateExp={calculateExp} />
+        <ExponentFour count={count} calculateExp={calculateExp} />
+        <ExponentFive count={count} calculateExp={calculateExp} />
+        <ExponentSix count={count} calculateExp={calculateExp} />
       </div>
     </div>
   );
